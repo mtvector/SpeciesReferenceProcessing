d82af09a14317e4085445d29f76e89966a8db6a7
Matthew Schmitz <matthew.schmitz@hpc-login.corp.alleninstitute.org>
2023-10-23 12:33:15 -0700

    ArchR Reference Builder Script

---
+++ ArchR/BuildAllArchRGenomesFromCellranger.R
@@ -0,0 +1,240 @@
+#This script processes a CSV file containing genome annotation directory 
+#information and generates ArchR reference files for ATAC-seq analysis.
+
+#Use singularity /allen/programs/celltypes/workgroups/rnaseqanalysis/bicore/singularity/10x_multiome_qc_4.2.0.sif
+#You need to have UCSC tools directory or give full path for faToTwoBit
+#write("TMP = '/home/matthew.schmitz/Matthew/tmp/'", file=file.path(Sys.getenv('R_USER'), '.Renviron'))
+#.libPaths(new = '/home/matthew.schmitz/R')
+.libPaths(new = '/home/matthew.schmitz/Matthew/utils/R')
+#Set the working directory to where you want the genome and org.db package to go
+intermediate_path <- '~/Matthew/archr_genomes/'
+setwd(intermediate_path)
+#CSV of the genomes spreadsheet
+tab=read.csv2('~/Reference_Genome_tracking.csv',header = T,sep=',')
+tab=tab[file.exists(tab$CR6.ARC.2.0.reference.in.BICore.folder),]
+
+#English name
+en_val <- 'English.Name'
+#Scientific name
+sp_val <- 'Species'
+#Directory to cellranger ref top dir
+dir_val <- 'CR6.ARC.2.0.reference.in.BICore.folder'
+#ref version name
+rn_val <- 'Top.NCBI.assembly'
+#URL where reference was downloaded from
+url_val <- 'RefSeq.anno.for.CR6'
+#assembly date
+ad_val <- 'Assembly.Date'
+#species ID
+spid_val <- 'NCBI.TaxID'
+#provider
+pr_val <- 'NCBI'
+#subpath to the genome fasta file beneath dir_val
+fa_subpath <- 'fasta/genome.fa'
+#subpath to the gtf/gff file beneath dir_val (gff is untested)
+gtf_subpath <- "genes/genes.gtf.gz"
+
+if (!requireNamespace("BSgenome")){
+  BiocManager::install("BSgenome",lib="/allen/programs/celltypes/workgroups/rnaseqanalysis/EvoGen/Team/Matthew/utils/R", force=T)
+}
+if (!requireNamespace("AnnotationForge")){
+  BiocManager::install("AnnotationForge",lib="/allen/programs/celltypes/workgroups/rnaseqanalysis/EvoGen/Team/Matthew/utils/R", force=T)
+}
+library(BSgenome)
+library(Biostrings)
+library(rtracklayer)
+library(AnnotationForge)
+library(ArchR)
+library(GenomicFeatures)
+library(biomaRt)
+library(httr)
+
+source('/allen/programs/celltypes/workgroups/rnaseqanalysis/EvoGen/Team/Matthew/code/SpeciesReferenceProcessing/ArchR/createGeneAnnotationUnsupportedSpecies.R')
+
+smart_join <- function(...) {
+  # Concatenate all arguments with a single slash
+  joined_path <- paste(..., sep = "/")
+  
+  # Remove any double slashes
+  cleaned_path <- gsub("//", "/", joined_path)
+  
+  return(cleaned_path)
+}
+
+parse_species_string <- function(input_string,sep="\\.") {
+  parts <- unlist(strsplit(input_string, sep))
+  initials <- substr(parts[-length(parts)], 1, 1)
+  output <- paste0(tolower(paste(initials, collapse = "")), tolower(parts[length(parts)]))
+  return(output)
+}
+
+create_seed_file <- function(english_name,species_name,assembly_date,
+                             ref_name,ref_url,seqfile_name,
+                             seqs_srcdir,ref_path,provider=pr_val) {
+  
+  package_name <- gsub(" ", ".", species_name)
+  #Make the string to write the seed file
+  seed_content <- paste0(
+    "Package: ", package_name, "\n",
+    "Title: ", species_name," (",english_name, ") genome\n",
+    "Description: ", english_name, " genome built from 2bit file\n",
+    "Version: 0.1.0\n",
+    "organism: ", species_name, "\n",
+    "common_name: ", english_name, "\n",
+    "provider: ",provider,"\n",
+    "genome: ", ref_name, "\n",
+    "release_date: ", assembly_date, "\n",
+    "source_url: ", ref_url, "\n",
+    "seqfile_name: ", seqfile_name, "\n",
+    "seqs_srcdir: ", seqs_srcdir, "\n",
+    "organism_biocview: ", gsub(" ", ".", species_name), "\n",
+    "BSgenomeObjname: ", gsub(" ", ".", species_name), "\n",
+    "circ_seqs: character(0)"
+  )
+  
+  # Write seed content to seed.txt file
+  out_path=paste0(ref_path, gsub(" ", ".", species_name), "_seed.txt")
+  write.table(seed_content, out_path,sep='\t',col.names=FALSE,quote=F,row.names = F)
+  return(c(out_path,package_name))
+
+}
+
+for (rn in rownames(tab)) {
+  cur=tab[rn,]
+  archref_files <- list.files(path = cur[[dir_val]], pattern = "_ArchRef\\.RData")
+  
+  # Check if any such files exist
+  if (length(archref_files) > 0) {
+    message(paste0("File(s) containing '_ArchRef.RData' found in ",cur[[dir_val]]))
+  } else {
+    
+  print(cur[[sp_val]])
+  print(cur[[dir_val]])
+  fa_in=smart_join(cur[[dir_val]],fa_subpath)
+  twob_out=gsub('\\.fa','.2bit',x=fa_in)
+  command=paste("faToTwoBit",fa_in,twob_out)
+  system(paste0("bash -c 'source ~/.bashrc;",command,"'"), intern = TRUE)
+  output_elements=create_seed_file(english_name=cur[[en_val]],
+                   species_name=cur[[sp_val]],
+                   provider=pr_val,
+                   assembly_date=cur[[ad_val]],
+                   ref_name=cur[[rn_val]],
+                   ref_url=cur[[url_val]],
+                   seqfile_name=basename(twob_out),
+                   seqs_srcdir=dirname(twob_out),
+                   ref_path=cur[[dir_val]]
+                   )
+  print('making bsg')
+  package_path=smart_join('./',output_elements[2])
+  
+  if (dir.exists(package_path) & output_elements[2]!="") {
+    print(paste("already exists so recurively deleting package_path", package_path))
+    unlink(package_path, recursive = TRUE)
+  }
+  
+  bsg <- BSgenome::forgeBSgenomeDataPkg(output_elements[1])
+  print('load bsg')
+  print(output_elements[2])
+  tools:::.build_packages(output_elements[2])
+  print(package_path)
+  genome_package_env <- devtools::load_all(package_path)
+  description_file <- smart_join(package_path, 'DESCRIPTION')
+  package_info <- read.dcf(description_file)
+  genomeAnnotation <- createGenomeAnnotation(genome = genome_package_env$env[[output_elements[2]]])
+  
+  print('gtf')
+  txdb <- GenomicFeatures::makeTxDbFromGFF(file = smart_join(cur[[dir_val]],gtf_subpath), 
+                          format="gtf",organism = cur[[sp_val]],taxonomyId = cur[[spid_val]])
+  genes <- GenomicFeatures::genes(txdb)
+  genus_name <- strsplit(cur[[sp_val]],split=" ")[[1]][1]
+  species_name <- strsplit(cur[[sp_val]],split=" ")[[1]][2]
+  print('make org')
+  orgDBname <- paste0("org.",toupper(substr(genus_name,1,1)),species_name,".eg.db")
+  org_path=smart_join('./',orgDBname,'/')
+  
+  if (dir.exists(org_path) & orgDBname!="") {
+    print(paste("already exists, consider deleting org_path to rebuild package:", org_path))
+    #print(unlink(org_path, recursive = TRUE))
+  }else{
+  #cache must be rebuilt each time unless you know which require
+  #ARCHIVE NCBI version or not (requires unigene)
+  #https://github.com/Bioconductor/AnnotationForge/issues/13
+  AnnotationForge::makeOrgPackageFromNCBI(
+    version = "0.1",
+    author = "EvoGen <EvoGenes@evogen.edu>",
+    maintainer = "EvoGen <EvoGenes@evogen.edu>",
+    outputDir = ".",
+    tax_id = cur[[spid_val]],
+    genus = genus_name,
+    species = species_name,
+    rebuildCache = TRUE
+  )
+
+  #   tryCatch(
+  #     {
+  #       # First block of code to try
+  #       AnnotationForge::makeOrgPackageFromNCBI(
+  #         version = "0.1",
+  #         author = "EvoGen <EvoGenes@evogen.edu>",
+  #         maintainer = "EvoGen <EvoGenes@evogen.edu>",
+  #         outputDir = ".",
+  #         tax_id = cur[[spid_val]],
+  #         genus = genus_name,
+  #         species = species_name,
+  #         rebuildCache = FALSE
+  #       )
+  #     },
+  #     error = function(e) {
+  #       # Code to run if an error occurs in the first block
+  #       message("Error encountered: ", e$message, ". Trying with rebuildCache = TRUE.")
+  #       AnnotationForge::makeOrgPackageFromNCBI(
+  #         version = "0.1",
+  #         author = "EvoGen <EvoGenes@evogen.edu>",
+  #         maintainer = "EvoGen <EvoGenes@evogen.edu>",
+  #         outputDir = ".",
+  #         tax_id = cur[[spid_val]],
+  #         genus = genus_name,
+  #         species = species_name,
+  #         rebuildCache = TRUE
+  #       )
+  #     }
+  #   )
+   }
+  
+  org_package_env <- devtools::load_all(org_path)
+  org_description_file <- smart_join(org_path, 'DESCRIPTION')
+  org_package_info <- read.dcf(org_description_file)
+  org_db <- org_package_env$env[[orgDBname]]
+  
+  
+  print(org_db)
+  ## Using a custom version of createGeneAnnotation that takes a data.frame to map gene_id to hgnc_symbol
+  httr::set_config(httr::config(ssl_verifypeer = FALSE))
+  ensembl <- useMart("ensembl", dataset=paste0(parse_species_string(output_elements[2]),"_gene_ensembl")) ##listDatasets(ensembl)
+  mart.genes <- getBM(attributes=c("ensembl_gene_id", "entrezgene_id", "hgnc_symbol"), values=keys(txdb), mart=ensembl)
+  #Not sure when this would be helpful but leaving it just in case
+  #seqlevels(txdb) <- paste0(c(seq(1,18), "X", "Y"))
+  #seqlevels(txdb) <- paste0("chr", seqlevels(txdb))
+  ## Using a custom version of createGeneAnnotation that takes a data.frame to map gene_id to hgnc_symbol
+  geneAnnotation <- createGeneAnnotationUnsupportedSpecies(genome = bsg,TxDb = txdb,
+                                                           OrgDb = org_db,
+                                                           mapping=mart.genes,
+                                                           annoStyle = "MANUAL")  
+  ## Remove genes without symbol
+  loci <- grep("NA", geneAnnotation$genes$symbol)
+  gid <- geneAnnotation$genes$gene_id[-loci]
+  df <- select(txdb, keys = gid, columns="TXNAME", keytype="GENEID")
+  
+  genes <- geneAnnotation$genes[-loci]
+  exons <- geneAnnotation$exons[-grep("NA", geneAnnotation$exons$symbol)]
+  tss <- geneAnnotation$TSS[which(geneAnnotation$TSS$tx_name %in% df$TXNAME)]
+  
+  ## Finalize annotation
+  geneAnnotationSubset <- createGeneAnnotation(genes = genes, 
+                                               exons = exons, 
+                                               TSS = tss)
+  
+  save(genomeAnnotation, geneAnnotation, geneAnnotationSubset, 
+       file = smart_join(cur[[dir_val]],paste0(output_elements[2],"_ArchRef.RData")))
+  }
+}
---
+++ ArchR/createGeneAnnotationUnsupportedSpecies.R
@@ -0,0 +1,185 @@
+#' Create a gene annotation object for ArchR
+#' 
+#' This function will create a gene annotation object that can be used for creating ArrowFiles or an ArchRProject, etc.
+#' 
+#' @param genome A string that specifies the genome (ie "hg38", "hg19", "mm10", "mm9"). If `genome` is not supplied,
+#' `TxDb` and `OrgDb` are required. If genome is supplied, `TxDb` and `OrgDb` will be ignored.
+#' @param TxDb A `TxDb` object (transcript database) from Bioconductor which contains information for gene/transcript coordinates.
+#' For example, from `txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene`.
+#' @param mapping Dataframe that maps between gene_id and gene_symbol typically from biomaRt as show in the example.
+#' @param OrgDb An `OrgDb` object (organism database) from Bioconductor which contains information for gene/transcript symbols from ids.
+#' For example, from `orgdb <- org.Hs.eg.db`.
+#' @param genes A `GRanges` object containing gene coordinates (start to end). Must have a symbols column matching the symbols column of `exons`.
+#' @param exons A `GRanges` object containing gene exon coordinates. Must have a symbols column matching the symbols column of `genes`.
+#' @param TSS A `GRanges` object containing standed transcription start site coordinates for computing TSS enrichment scores downstream.
+#' @param annoStyle annotation style to map between gene names and various gene identifiers e.g. "ENTREZID", "ENSEMBL".
+#'
+#' @example 
+#'
+#' genomeAnnotation <- createGenomeAnnotation(genome = "BSgenome.Sscrofa.UCSC.susScr11")
+#' txdb <- makeTxDbFromEnsembl(organism = "Sus Scrofa")
+#'
+#' ## Manual mapping between gene_id and symbol for "unsupported" species
+#' library("biomaRt")                                                                                                                            
+#' httr::set_config(httr::config(ssl_verifypeer = FALSE))                                                                                                                
+#' ensembl <- useMart("ensembl", dataset="sscrofa_gene_ensembl") ##listDatasets(ensembl)                                                                     
+#' mart.genes <- getBM(attributes=c("ensembl_gene_id", "entrezgene_id", "hgnc_symbol"), values=keys(txdb), mart=ensembl)   
+#'
+#' seqlevels(txdb) <- paste0(c(seq(1,18), "X", "Y"))
+#' seqlevels(txdb) <- paste0("chr", seqlevels(txdb))
+#'
+#' ## Using a custom version of createGeneAnnotation that takes a data.frame to map gene_id to hgnc_symbol
+#' geneAnnotation <- createGeneAnnotationUnsupportedSpecies(TxDb = txdb,
+#'                                                          mapping = mart.genes,
+#'                                                          OrgDb = org.Ss.eg.db)
+#'
+#' ## Remove genes without symbol
+#' loci <- grep("NA", geneAnnotation$genes$symbol)
+#' gid <- geneAnnotation$genes$gene_id[-loci]
+#' df <- select(txdb, keys = gid, columns="TXNAME", keytype="GENEID")
+#'
+#' genes <- geneAnnotation$genes[-loci]
+#' exons <- geneAnnotation$exons[-grep("NA", geneAnnotation$exons$symbol)]
+#' tss <- geneAnnotation$TSS[which(geneAnnotation$TSS$tx_name %in% df$TXNAME)]
+#'
+#' ## Finalize annotation
+#' geneAnnotationSubset <- createGeneAnnotation(genes = genes, 
+#'                                              exons = exons, 
+#'                                              TSS = tss)
+#' save(genomeAnnotation, geneAnnotation, geneAnnotationSubset, file = "susScr11.RData")
+#'
+#' @export
+createGeneAnnotationUnsupportedSpecies <- function(
+  genome = NULL,
+  TxDb = NULL,
+  mapping = NULL,
+  OrgDb = NULL,
+  genes = NULL,
+  exons = NULL,
+  TSS = NULL,
+  annoStyle = NULL
+  ){
+
+  # .validInput(input = genome, name = "genome", valid = c("character", "null"))
+  # .validInput(input = TxDb, name = "TxDb", valid = c("txdb", "character", "null"))
+  # .validInput(input = OrgDb, name = "OrgDb", valid = c("orgdb", "character", "null"))
+  # .validInput(input = genes, name = "genes", valid = c("GRanges", "null"))
+  # .validInput(input = exons, name = "exons", valid = c("GRanges", "null"))
+  # .validInput(input = TSS, name = "TSS", valid = c("GRanges", "null"))
+  # .validInput(input = annoStyle, name = "annoStyle", valid = c("character", "null"))
+
+  if(is.null(genes) | is.null(exons) | is.null(TSS)){
+
+    inGenes <- genes
+    inExons <- exons
+    inTSS <- TSS
+
+    # .requirePackage("GenomicFeatures", source = "bioc")
+    library(GenomicFeatures)
+
+    # if(is.null(genome)) {
+    #   if (is.null(TxDb) | is.null(OrgDb)) {
+    #       stop("If no provided genome then you need TxDb and OrgDb!")
+    #   }
+    # }
+
+    if(!is.null(genome)){
+      TxDb <- .getTxDb(genome)
+      OrgDb <- .getOrgDb(genome)
+    }
+
+    ###########################
+    message("Getting Genes..")
+    genes <- GenomicFeatures::genes(TxDb)
+
+    if(is.null(annoStyle)){
+      isEntrez <- mcols(genes)$symbol <- tryCatch({
+        suppressMessages(AnnotationDbi::mapIds(OrgDb, keys = mcols(genes)$gene_id, column = "SYMBOL", keytype = "ENTREZID", multiVals = "first"))
+        TRUE
+      }, error = function(x){
+        FALSE
+      })
+
+      isEnsembl <- mcols(genes)$symbol <- tryCatch({
+        suppressMessages(AnnotationDbi::mapIds(OrgDb, keys = mcols(genes)$gene_id, column = "SYMBOL", keytype = "ENSEMBL", multiVals = "first"))
+        TRUE
+      }, error = function(x){
+        FALSE
+      })
+
+      if(isEntrez){
+        annoStyle <- "ENTREZID"
+      }else if(isEnsembl){
+        annoStyle <- "ENSEMBL"
+      }else{
+        if(!is.null(mapping)){
+          annoStyle <- "MANUAL"
+        }else{
+          stop("Could not identify keytype for annotation format!")
+        }
+      }
+    }
+    annoStyle <- toupper(annoStyle)
+
+    message("Determined Annotation Style = ", annoStyle)
+
+    ###########################
+    if(annoStyle == "MANUAL"){
+      mcols(genes)$symbol = mapping$hgnc_symbol[match(mcols(genes)$gene_id, mapping$ensembl_gene_id)]
+      mcols(genes)$symbol[mcols(genes)$symbol == ""] = NA
+      mcols(genes)$symbol[is.na(mcols(genes)$symbol)] <- paste0("NA_", mcols(genes)$gene_id)[is.na(mcols(genes)$symbol)]
+    }else{
+      mcols(genes)$symbol <- suppressMessages(AnnotationDbi::mapIds(OrgDb, keys = mcols(genes)$gene_id, column = "SYMBOL", keytype = annoStyle, multiVals = "first"))
+      mcols(genes)$symbol[is.na(mcols(genes)$symbol)] <- paste0("NA_", mcols(genes)$gene_id)[is.na(mcols(genes)$symbol)]
+    }
+    names(genes) <- NULL
+    genes <- sort(sortSeqlevels(genes), ignore.strand = TRUE)
+
+    ###########################
+    message("Getting Exons..")
+    exons <- unlist(GenomicFeatures::exonsBy(TxDb, by = "tx"))
+    exons$tx_id <- names(exons)
+    mcols(exons)$gene_id <- suppressMessages(AnnotationDbi::select(TxDb, keys = paste0(mcols(exons)$tx_id), column = "GENEID", keytype = "TXID")[, "GENEID"])
+    exons <- exons[!is.na(mcols(exons)$gene_id), ]
+    if(annoStyle == "MANUAL"){
+      mcols(exons)$symbol = mapping$hgnc_symbol[match(mcols(exons)$gene_id, mapping$ensembl_gene_id)]
+      mcols(exons)$symbol[mcols(exons)$symbol == ""] = NA
+      mcols(exons)$symbol[is.na(mcols(exons)$symbol)] <- paste0("NA_", mcols(exons)$gene_id)[is.na(mcols(exons)$symbol)]
+    }else{
+      mcols(exons)$symbol <- suppressMessages(AnnotationDbi::mapIds(OrgDb, keys = mcols(exons)$gene_id, column = "SYMBOL", keytype = annoStyle, multiVals = "first"))
+      mcols(exons)$symbol[is.na(mcols(exons)$symbol)] <- paste0("NA_", mcols(exons)$gene_id)[is.na(mcols(exons)$symbol)]
+    }
+    names(exons) <- NULL
+    mcols(exons)$exon_id <- NULL
+    mcols(exons)$exon_name <- NULL
+    mcols(exons)$exon_rank <- NULL
+    mcols(exons)$tx_id <- NULL
+    exons <- sort(sortSeqlevels(exons), ignore.strand = TRUE)
+
+    ###########################
+    message("Getting TSS..")
+    TSS <- unique(resize(GenomicFeatures::transcripts(TxDb), width = 1, fix = "start"))
+
+    # if(!is.null(inGenes)){
+    #   genes <- .validGRanges(inGenes)
+    # }
+
+    # if(!is.null(inExons)){
+    #   exons <- .validGRanges(inExons)
+    # }
+
+    # if(!is.null(inTSS)){
+    #   TSS <- .validGRanges(inTSS)
+    # }
+
+  }else{
+
+    genes <- .validGRanges(genes)
+    exons <- .validGRanges(exons)
+    TSS <- unique(.validGRanges(TSS))
+
+  }
+
+  SimpleList(genes = genes, exons = exons, TSS = TSS)
+
+}
